c29c06026e9d1f0873c32445a3b0176f3a896c4b
Steffengreiner <Steffen.Greiner@gmx.de>
2021-06-28 15:40:20 +0200

    Fix div in template and increase maxtableitems

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/offergeneration/QuotationDetails.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/offergeneration/QuotationDetails.groovy
@@ -43,7 +43,7 @@ class QuotationDetails {
     /**
      * The maximum number of items per page
      */
-    private final int maxPageItems = 25
+    private final int maxPageItems = 27
 
     /**
      * Product group mapping
